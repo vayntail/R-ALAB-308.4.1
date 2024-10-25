30156baba90bc6904d4221e517db4be48a34e0c9
vayntail <113714157+vayntail@users.noreply.github.com>
2024-10-25 15:59:17 -0400

    Update

---
+++ lab.js
@@ -0,0 +1,105 @@
+const CSV_STR = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+// ======== Part 1: Refactoring Old Code ========
+// let word = '';
+// let row = [];
+
+// for (let i = 0; i < CSV_STR.length; i++) {
+//     let ch = CSV_STR[i];
+
+//     if (ch != "," && ch != "\n") {
+//         word += ch; // add character to word
+//     }
+//     else {
+//         row.push(word); // add to row
+//         word = ''; // clear word
+
+//         if (ch == "\n") {
+//             // print out the row
+//             console.log(row.join(', '));
+//             // clear row
+//             row = [];
+//         }
+//     } 
+// }
+
+// ======== Part 2: Expanding Functionality ========
+// let word = '';
+// let data = [];
+// let row = [];
+
+// for (let i = 0; i < CSV_STR.length; i++) {
+//     let ch = CSV_STR[i];
+
+//     if (ch != "," && ch != "\n") {
+//         word += ch; // add character to word
+//     }
+//     else {
+//         row.push(word); // add to row
+//         word = ''; // clear word
+
+//         if (ch == "\n") {
+//             // add row array to data array
+//             data.push(row);
+//             // clear row
+//             row = [];
+//         }
+//     } 
+// }
+// // Print out data
+// console.log(data);
+
+// ======== Part 3: Transforming Data ========
+let word = '';
+let row = [];
+let data = [];
+
+for (let i = 0; i < CSV_STR.length; i++) {
+    let ch = CSV_STR[i];
+
+    if (ch != "," && ch != "\n") {
+        word += ch; // add character to word
+    }
+
+    else {
+        console.log(ch);
+        row.push(word); // add to row
+        word = ''; // clear word
+
+        // Check if new line, or at the end of the string.
+        if (ch == "\n") {
+            // add row array to data array
+            data.push(row);
+            // clear row
+            row = [];
+        }
+    }
+}
+// Go through the first row inside data to find headings.
+let firstRow = data[0];
+let headings = []; // first row, but in lower case
+let objectsData = [];
+// Loop through each word inside the first row and add the lower case version of it to headings array.
+firstRow.forEach((word) => headings.push(word.toLowerCase()));
+
+// Start looping through data at index 1 since we don't want the first line (headings)
+for (let i = 1; i < data.length; i++) {
+    let row = data[i];
+
+    // Make object that looks like: { id: "42", name: "Bruce", occupation: "Knight", age: "41" }
+
+    let obj = {}; // Make blank object
+
+    // Iterate through the amount of columns
+    for (let i = 0; i < headings.length; i++) {
+        obj[headings[i]] = row[i]; // add key:value to object
+    }
+
+    objectsData.push(obj);
+}
+
+// Print objects Array
+console.log(objectsData);
+
+
+
