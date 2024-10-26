4293220aefa2ef8a7c90efa090d55413c6a7d301
vayntail <113714157+vayntail@users.noreply.github.com>
2024-10-26 17:07:54 -0400

    Final Push

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
 const CSV_STR = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-console.log(CSV_STR.split('\n'));
+let bigArray = []; // creates an empty array
+let arr = CSV_STR.split('\n');
+
+// loops through each string in array (USING FOREACH)
+// arr.forEach((string) => {
+//     bigArray.push(string.split(',')) // add to array
+// })
+
+// (USING WHILE)
+let index = 0;
+while (index < arr.length){
+    bigArray.push(arr[index].split(',')) // add to array
+    index += 1;
+    console.log(index)
+}
+console.log(bigArray)
